0e72dc239c73831dd58fa6d5045485393723fbe7
Krystian Petek <krystianpetek2@gmail.com>
2023-01-02 17:03:02 +0100

    Added configurationString to MS SQL Server Added script for run MS SQL Server in docker container Added EntityFrameworkCore packages

---
+++ script.ps1
@@ -0,0 +1,3 @@
+docker pull mcr.microsoft.com/mssql/server
+
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=NotAll0wedForPublic" -p 8720:1433 -d --name webshop-catalog-sql mcr.microsoft.com/mssql/server:latest
