8a98293437adf37eca02af1301b7e9b99b2a0c5a
Krystian Petek <krystianpetek2@gmail.com>
2023-01-07 19:15:10 +0100

    Added validationBehavior which will be working before request handler to every command for prevent failures and also added UnhandledException method to MediatR Pipeline

---
+++ KriniteWebShop.ProductOrder.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace KriniteWebShop.ProductOrder.Application.Behaviors;
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (_validators.Any())
+        {
+            ValidationContext<TRequest> context = new ValidationContext<TRequest>(request);
+
+            ICollection<ValidationResult> validationRequests = await Task.WhenAll(
+                _validators.Select(validate => validate.ValidateAsync(context, cancellationToken)));
+
+            ICollection<ValidationFailure> failures = validationRequests.SelectMany(error => error.Errors).Where(error => error != null).ToList();
+
+            if (failures.Count != 0)
+                throw new ValidationException(failures);
+        }
+
+        return await next();
+    }
+}
