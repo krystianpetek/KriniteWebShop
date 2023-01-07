6cef2336e01aa91b749c784f4bc450efcb5c16ba
Krystian Petek <krystianpetek2@gmail.com>
2023-01-07 08:04:50 +0100

    Prepared Application layer class like repository, services, mappings, settings and partitially installed required nuget packages to implement CQRS

---
+++ KriniteWebShop.ProductOrder.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿using KriniteWebShop.ProductOrder.Application.Models;
+
+namespace KriniteWebShop.ProductOrder.Application.Contracts.Infrastructure;
+internal interface IEmailService
+{
+    Task<bool> SendMail(Email email);
+}
---
+++ KriniteWebShop.ProductOrder.Application/Models/EmailSettings.cs
@@ -0,0 +1,8 @@
+﻿
+namespace KriniteWebShop.ProductOrder.Application.Models;
+public class EmailSettings
+{
+    public string ApiKey { get; set; }
+    public string FromAddress { get; set; }
+    public string FromName { get; set; }
+}
