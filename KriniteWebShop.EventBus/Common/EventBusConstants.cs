3c5607becdb3ee29856818a90316e8a5ad424608
Krystian Petek <krystianpetek2@gmail.com>
2023-01-09 19:22:01 +0100

    Initialized EventBus class library to hold events class which will be used from few other projects in solution Added RabbitMq broker in docker compose deploy based on linux alpine with management dashboard

---
+++ KriniteWebShop.EventBus/Common/EventBusConstants.cs
@@ -0,0 +1,7 @@
+﻿using KriniteWebShop.EventBus.Events;
+
+namespace KriniteWebShop.EventBus.Common;
+public static class EventBusConstants
+{
+    public const string CartCheckoutQueue = $"{nameof(CartCheckoutEvent)}-queue";
+}
---
+++ KriniteWebShop.EventBus/Events/SharedBaseEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace KriniteWebShop.EventBus.Events;
+public class SharedBaseEvent
+{
+    public Guid Id { get; private set; }
+    public DateTime CreationDate { get; private set; }
+
+    public SharedBaseEvent(Guid id, DateTime creationDate)
+    {
+        Id = id;
+        CreationDate = creationDate;
+    }
+
+    public SharedBaseEvent()
+    {
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.UtcNow;
+    }
+}
+ 
