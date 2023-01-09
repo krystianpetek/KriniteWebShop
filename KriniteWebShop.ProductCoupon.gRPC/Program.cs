9b4e2bbae03a14e10a87eea47f271bae8895b967
Krystian Petek <krystianpetek2@gmail.com>
2023-01-09 05:48:00 +0100

    Added ProductCoupon.gRPC to deploy using docker compose with every necessery settings/change

--- KriniteWebShop.ProductCoupon.gRPC/Program.cs
+++ KriniteWebShop.ProductCoupon.gRPC/Program.cs
@@ -18,7 +18,7 @@ public class Program
 
         app.MapGrpcService<CouponService>();
         app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
+        
         app.SeedData();
 
         app.Run();
