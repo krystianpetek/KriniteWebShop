0140bb5ffcb2b32a3ff0953840aed429bbe8fd90
Krystian Petek <krystianpetek2@gmail.com>
2023-01-17 20:24:11 +0100

    Added models to WebClient app Defined GatewayApiUrl address

---
+++ KriniteWebShop.WebClient/Models/CartModel.cs
@@ -0,0 +1,9 @@
+﻿namespace KriniteWebShop.WebClient.Models;
+
+public class CartModel
+{
+    public ICollection<CartItemModel>? Items { get; set; } = new List<CartItemModel>();
+
+    public decimal? TotalPrice { get; set; }
+
+}
