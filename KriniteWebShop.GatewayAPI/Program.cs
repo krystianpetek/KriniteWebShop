4b7b7f6e2da34413d7f4ea2e99b84cc5a7985d3a
Krystian Petek <krystianpetek2@gmail.com>
2023-01-19 20:26:11 +0100

    Added Swagger to Gateway api project but for now WIP, i will later to this feature

--- KriniteWebShop.GatewayAPI/Program.cs
+++ KriniteWebShop.GatewayAPI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
@@ -15,12 +16,36 @@ public class Program
             .AddConsole()
             .AddDebug();
 
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen((swagger) =>
+        {
+            swagger.SwaggerDoc(
+                name: "v1",
+                info: new OpenApiInfo
+                {
+                    Title = "GatewayAPI",
+                    Version = "v1"
+                });
+        });
+
         builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
 
         builder.Services.AddOcelot()
             .AddCacheManager(cacheManager => cacheManager.WithDictionaryHandle());
 
         var app = builder.Build();
+        app.UseCors(cors =>
+        {
+            cors.AllowAnyOrigin();
+        });
+        app.UseSwagger();
+        app.UseSwaggerUI(swagger =>
+        {
+            swagger.SwaggerEndpoint(
+                    url: "http://localhost:5821/swagger/v1/swagger.json",
+                    name: "api v1");
+
+        });
 
         await app.UseOcelot();
 
