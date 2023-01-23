6f9428a03b49155fd07d75d9fdaf36ad5d80d76f
Krystian Petek <krystianpetek2@gmail.com>
2023-01-23 20:30:08 +0100

    Fixed partial ProductItem component Added cart page with call to backend API Changed grid sizing in IndexPage to display products in proper position

--- KriniteWebShop.WebClient/Pages/Cart.cshtml.cs
+++ KriniteWebShop.WebClient/Pages/Cart.cshtml.cs
@@ -1,3 +1,5 @@
+using KriniteWebShop.WebClient.Models;
+using KriniteWebShop.WebClient.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,7 +7,21 @@ namespace KriniteWebShop.WebClient.Pages;
 
 public class CartModel : PageModel
 {
-    public void OnGet()
+    private readonly ICartService _cartService;
+    private readonly ILogger<CartModel> _logger;
+    public Models.CartModel _cartModel { get; set; }
+
+    public string UserName { get; set; } = "krystianpetek2";
+
+    public CartModel(ILogger<CartModel> logger, ICartService cartService, IProductService productService)
     {
+        _logger = logger;
+        _cartService = cartService ?? throw new ArgumentNullException(nameof(cartService));
+    }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        _cartModel = await _cartService.GetCartAsync(UserName);
+        return Page();
     }
 }
